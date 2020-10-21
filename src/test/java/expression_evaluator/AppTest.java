d3412ad90d01648c684566d79a6e6efead5ff3ac
Felix Tan <felixtan92@gmail.com>
2020-10-21 17:13:07 +0800

    Clean imports

--- src/test/java/expression_evaluator/AppTest.java
+++ src/test/java/expression_evaluator/AppTest.java
@@ -3,8 +3,8 @@
  */
 package expression_evaluator;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+// import org.junit.Test;
+// import static org.junit.Assert.*;
 
 public class AppTest {
     // @Test public void testAppHasAGreeting() {
